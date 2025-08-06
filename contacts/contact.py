1e303e574653a5f3b27fcf7dde5785b207c33d75
Kweza <thomaslorilpro@gmail.com>
2025-08-06 19:51:33 +0200

    Adding new features: ViewALL contacts, addContact

--- contacts/contact.py
+++ contacts/contact.py
@@ -4,12 +4,12 @@ import os
 
 class Contact:
     def __init__(self, name, email, phone):
-        name = self.name
-        email = self.email
-        phone = self.email
+        self.name = name
+        self.email = email
+        self.phone = phone
 
     def to_dict(self):
-        data = {
+        return {
             "name": self.name,
             "email": self.email,
             "phone": self.phone
@@ -23,19 +23,57 @@ def display_menu():
     print("3 : Exit")
 
 
-def add_contact():
+def add_contact(file_name):
+    file_name = "contact.json"
     name = input("Please enter the name of your new contact: ")
     email = input("Please enter the email of your new contact: ")
     phone = int(input("Please enter the phone number of your new contact"))
 
     contact = Contact(name, email, phone)
-    file_name = "contact.json"
+
     if os.path.exists(file_name):
         with open(file_name, "r", encoding="UTF-8") as file:
             try:
-                contact = json.load(file)
+                datas = json.load(file)
             except json.JSONDecodeError:
-                data = []
+                datas = []
 
             except Exception as e:
                 print(f"An error as error has occured {e} ")
+
+    datas.append(contact.to_dict())
+
+    with open(file_name, "w", encoding="UTF-8") as file:
+        json.dump(datas, file, indent=4)
+
+    print("Contact added with sucesss")
+
+
+def viewAll(file_name):
+
+    if os.path.exists(file_name):
+        with open(file_name, "r", encoding="UTF-8") as file:
+            datas = json.load(file)
+
+    for idx, data in enumerate(datas, start=1):
+        print(f"{idx}: {data}")
+
+
+def main():
+
+    file_name = "contact.json"
+
+    while True:
+        display_menu()
+
+        try:
+            action = int(input("Please enter a number : "))
+            if action == 1:
+                add_contact(file_name)
+            elif action == 2:
+                viewAll(file_name)
+        except Exception as e:
+            (f"An error has occured {e}")
+
+
+main()
